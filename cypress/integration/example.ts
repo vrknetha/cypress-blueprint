66d4de47b5f9a464bc53d4fbd57a3ead4066c1b1
vrknetha <vrknetha@gmail.com>
2020-07-09 21:39:30 +0530

    added the workaround

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -1,7 +1,26 @@
 describe("Login tests", () => {
-    it("login using credentials", () => {
+	before(function () {
+		// run this once before all code
+		return window.caches.keys().then(function(cacheNames) {
+		  return Promise.all(
+			cacheNames.map(function(cacheName) {
+			  return window.caches.delete(cacheName);
+			})
+		  );
+		});
+	});
+
+    it("login using credentials", function() {
         cy.fixture(`user`).as('user');
-		cy.visit('https://develop.convosight.com/app/cs-admin-login');
+		cy.visit('https://develop.convosight.com/app/cs-admin-login', {
+			onBeforeLoad: function(win) {
+			  const promise = new Promise(function(resolve) {});
+			  //@ts-ignore
+			  return win.navigator.serviceWorker.register = function() {
+				return promise;
+			  }
+			}
+		  });
 		cy.get(`input[placeholder='Email']`).type(`@user.cs-admin.user`);
 		cy.get(`input[placeholder='Password']`).type(`@user.cs-admin.password`);
 		cy.get(`button[type="submit"]`).click();
